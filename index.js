4269d5de4bfe15b26297936440ffd0277f2ea4c9
Ragmatical <37642718+Ragmatical@users.noreply.github.com>
2018-09-10 18:03:46 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var Io = require('socket.io')
+var http = require('http');
+var usermodel = require('./user.js').getModel();
+var crypto = require('crypto');
+var mongoose = require('mongoose')
+var path = require('path');
+var passport = require('passport')
+var LocalStrategy = require('passport-local').Strategy
+var session = require('express-session');
+var fs = require('fs')
+var app = express();
+var server = http.createServer(app);
+var io = Io(server)
+var port = process.env.PORT ? parseInt(process.env.PORT) : 8080;
+
+
+function startServer() {
+  app.get('/', (req, res, next) => {
+    var filePath = path.join(__dirname, '/home.html')
+    res.sendFile(filePath)
+  })
+}
+
+startServer()
