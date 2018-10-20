697bcdb92db58929e9d77366ccd78cfea7b5e508
ragmatical <junokimzone@gmail.com>
2018-10-19 23:50:30 -0700

    trying to add bootstrap

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ var app = express();
 var server = http.createServer(app);
 var io = Io(server)
 var port = process.env.PORT ? parseInt(process.env.PORT) : 8080;
+var bootstrap = require('bootstrap')
 
 
 function startServer() {
